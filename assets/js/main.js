8e481228b7f726118ed18d4ea5337b3619f11b03
mccaffertycr <mccaffertycr@gmail.com>
2018-06-15 15:07:44 -0400

    styled using css grid (room for improvement here); not sure if gitcommit is functioning

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,165 @@
-var xobj = new XMLHttpRequest();
-xobj.overrideMimeType("application/json");
-xobj.open('GET', 'data.json', true);
-xobj.onreadystatechange = function () {
-if (xobj.readyState == 4 && xobj.status == "200") {
+    var rndm = "";
 
-// .open will NOT return a value but simply returns undefined in async mode so use a callback
-callback(xobj.responseText);
+    // create answer array
+    var answerArray = [];
 
+    // guess counter
+    var guessCounter = 20;
+
+    // guessed letters
+    var guessedLetters = [];
+
+    //guess display 
+    var guessDisplay = [];
+
+    // empty guess var
+    var userGuess = "";
+
+    var gameOver;
+
+    var winTimeout;
+
+    // function that changes answerarray to underscores & prints
+    function answerBlank(a) {
+        for (var i = 0; i < data.movies[rndm].length; i++) {
+            if (data.movies[rndm][i] === " ") {
+                a[i] = " ";
+            } else {
+                a[i] = "_";
+            }
+        }
+    }
+// win message function
+function winMsg() {
+    var queryURL = "https://www.omdbapi.com/?t=" + data.movies[rndm] + "&y=&plot=short&apikey=" + APIKEY;
+    $("#guess-left").text("YOU WIN!!");
+    $("#note").hide();
+    $("#game").empty();
+    $("#hint").empty();
+    $("#show-hint").hide();
+    $("#guessed").empty();
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+            console.log(response);
+            var title = $("<h2>").text(response.Title);
+            var rating = $("<p>").text(response.Rated);
+            var relDate = $("<p>").text("Rel: " + response.Released);
+            var dir = $("<p>").text(response.Director);
+            var poster = $("<img src=" + response.Poster + ">");
+            $("#guessed").append(title, rating, relDate, dir);
+            $("#gameover").append(poster); 
+          });
+    }
+
+
+// function to start game
+function startGame() {  
+
+    // empty the gameover msg
+    $("#gameover").empty();
+
+    // reprint the header
+    $("#note").show();
+
+    // pick a random title               
+    rndm = Math.floor(Math.random()*data.movies.length);
+
+    gameOver = false;
+
+    // show hint button
+    $("#show-hint").show();
+
+    // clears the hint
+    $("#hint").empty();
+
+    //clears win timeout
+    clearTimeout(winTimeout);
+
+    //reset answerArray to empty
+    answerArray = [];
+    
+    // fills answer array with underscores 
+    answerBlank(answerArray);
+
+    // print answer array to html
+    $("#game").text(answerArray.join(""));
+
+    // reset guesses
+    guessCounter = 20;
+
+    guessedLetters = [];
+
+     // Prints the blanks at the beginning of each round in the HTML.
+    $("#guessed").text("ALREADY GUESSED: " + (guessedLetters.join(" ")));
+
+    // reset the guess counter
+    $("#guess-left").text("GUESSES LEFT:" + guessCounter);
 }
+
+// starts game
+startGame();
+
+// function run when user starts guessing
+$(document).on("keyup", function(e) {
+
+    if (!gameOver) {
+    // determines guess
+    userGuess = e.key;
+     
+    // change answer array to reflect guess if it's correct
+    for (var g = 0; g < answerArray.length; g++) {
+        if (userGuess.toUpperCase() === data.movies[rndm][g].toUpperCase()) {
+            answerArray[g] = userGuess.toUpperCase();
+        }
+    } 
+    // re-print answer array with answerto html
+    $("#game").text(answerArray.join(""));
+
+    // save guesses to guessLetters
+    guessedLetters.push(userGuess.toUpperCase());
+    // display guessed letters
+    $("#guessed").text("ALREADY GUESSED: " + (guessedLetters.join("")));
+    // guessDisplay = $("guessed").text() += userGuess.toUpperCase();
+
+    // guess counter display
+   $("#guess-left").text("GUESSES LEFT:" + (guessCounter--));
 }
-xobj.send(null);
 
-
-
-// Call to function with anonymous callback
-loadJSON(function(response) {
-// Do Something with the response e.g.
-jsonresponse = JSON.parse(response);
-
-// Assuming json data is wrapped in square brackets as Drew suggests
-console.log(jsonresponse);
-
+    // restart function conditions
+    if (guessedLetters.length === 20) {
+        gameOver = true;
+        $("#game").text("OUT OF GUESSES, TRY AGAIN!");
+        setTimeout(function() {
+            startGame();  
+        }, 2500);
+    } else if (answerArray.join("") === data.movies[rndm].toUpperCase()) {
+        gameOver = true;
+        winMsg();
+        setTimeout(function() {
+            startGame();  
+        }, 10000);
+    }
 });
+
+   // connect button with relevant hint
+   $("#show-hint").on("click", function() {
+    $("#hint").empty();
+    var queryURL = "https://www.omdbapi.com/?t=" + data.movies[rndm] + "&y=&plot=short&apikey=" + APIKEY;
+
+    // Creates AJAX call for the specific movie button being clicked
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    if (data.movies[rndm] === response.Title) {
+        $("#hint").text(response.Plot);
+    }
+    });
+});
+
+
+
+
